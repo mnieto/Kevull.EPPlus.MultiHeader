d8785c87f83084ab8816700fea8e38befdf672cb
mnieto <m.nieto@kevull.com>
2023-11-10 19:44:25 +0100

    Add ColumnEnumeration type

--- src/EPPLus.MultiHeader.Test/ColumnInfoTest.cs
+++ src/EPPLus.MultiHeader.Test/ColumnInfoTest.cs
@@ -1,14 +1,22 @@
 ﻿using EPPLus.MultiHeader.Columns;
+using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace EPPLus.MultiHeader.Test
 {
     public class ColumnInfoTest
     {
+
+        public ColumnInfoTest()
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+        }
+
         [Fact]
         public void Order_MustBeOneOrUpper()
         {
@@ -25,5 +33,112 @@ namespace EPPLus.MultiHeader.Test
             var sut = new ColumnInfo(nameof(Person.BirthDate));
             Assert.Equal(sut.Name, sut.DisplayName);
         }
+
+        [Fact]
+        public void ColumnEnumeration_Dicitionary_WritesOneColumPerKey()
+        {
+            var xls = new ExcelPackage();
+            xls.Workbook.Worksheets.Add("Enummeration");
+            var sheet = xls.Workbook.Worksheets["Enummeration"];
+            var data = new RiskDict
+            {
+                Name = "TestRisk",
+                Levels = new Dictionary<string, int>
+                {
+                    { "LOW", 10 },
+                    { "MED", 20 },
+                    { "HIGh", 30 }
+                }
+            };
+            var properties = typeof(RiskDict).GetProperties()
+                .ToDictionary(x => x.Name, x => x);
+
+            var column = new ColumnEnumeration("Levels", data.Levels.Keys);
+            column.WriteCell(sheet.Cells["B2"], properties, data);
+
+            Assert.Equal(10, sheet.GetValue<int>(2, 2));
+            Assert.Equal(20, sheet.GetValue<int>(2, 3));
+            Assert.Equal(30, sheet.GetValue<int>(2, 4));
+
+        }
+
+        [Fact]
+        public void ColumnEnumeration_Dicitionary_ThrowsWhenNotExpectedKey()
+        {
+            var xls = new ExcelPackage();
+            xls.Workbook.Worksheets.Add("Enummeration");
+            var sheet = xls.Workbook.Worksheets["Enummeration"];
+            var data = new RiskDict
+            {
+                Name = "TestRisk",
+                Levels = new Dictionary<string, int>
+                {
+                    { "LOW", 10 },
+                    { "MED", 20 },
+                    { "HIGh", 30 }
+                }
+            };
+            var properties = typeof(RiskDict).GetProperties()
+                .ToDictionary(x => x.Name, x => x);
+
+            var column = new ColumnEnumeration("Levels", data.Levels.Keys.Take(2));
+            Assert.Throws<KeyNotFoundException>(() => column.WriteCell(sheet.Cells["B2"], properties, data));
+        }
+
+        [Fact]
+        public void ColumnEnumeration_Enumberable_WritesOneColumPerKey()
+        {
+            var xls = new ExcelPackage();
+            xls.Workbook.Worksheets.Add("Enummeration");
+            var sheet = xls.Workbook.Worksheets["Enummeration"];
+            var data = new RiskList
+            {
+                Name = "TestRisk",
+                Levels = new List<int> { 10, 20, 30 }
+            };
+            var properties = typeof(RiskList).GetProperties()
+                .ToDictionary(x => x.Name, x => x);
+
+            var column = new ColumnEnumeration("Levels", data.Levels.ConvertAll(x => x.ToString()));
+            column.WriteCell(sheet.Cells["B2"], properties, data);
+
+            Assert.Equal(10, sheet.GetValue<int>(2, 2));
+            Assert.Equal(20, sheet.GetValue<int>(2, 3));
+            Assert.Equal(30, sheet.GetValue<int>(2, 4));
+
+        }
+
+        [Fact]
+        public void ColumnEnumeration_Enumerable_ThrowsWhenNotExpectedKey()
+        {
+            var xls = new ExcelPackage();
+            xls.Workbook.Worksheets.Add("Enummeration");
+            var sheet = xls.Workbook.Worksheets["Enummeration"];
+            var data = new RiskList
+            {
+                Name = "TestRisk",
+                Levels = new List<int> { 10, 20, 30 }
+            };
+            var properties = typeof(RiskList).GetProperties()
+                .ToDictionary(x => x.Name, x => x);
+
+            var column = new ColumnEnumeration("Levels", data.Levels.ConvertAll(x => x.ToString()).Take(2));
+            Assert.Throws<KeyNotFoundException>(() => column.WriteCell(sheet.Cells["B2"], properties, data));
+        }
+
+    }
+
+
+
+    internal class RiskDict
+    {
+        public string Name { get; set; }
+        public Dictionary<string, int> Levels { get; set; }
+    }
+
+    internal class RiskList
+    {
+        public string Name { get; set; }
+        public List<int> Levels { get; set; }
     }
 }
---
+++ src/EPPLus.MultiHeader/Columns/ColumnEnumeration.cs
@@ -0,0 +1,58 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPPLus.MultiHeader.Columns
+{
+    internal class ColumnEnumeration : ColumnInfo
+    {
+        private readonly Dictionary<string, int> _keyValues;
+        public ColumnEnumeration(string name, IEnumerable<string> keyValues, bool ignore) : base(name, ignore)
+        {
+            int i = 0;
+            _keyValues = keyValues.ToDictionary(x => x, _ => i++);
+        }
+
+        public ColumnEnumeration(string name, IEnumerable<string> keyValues, int? order = null, string? displayName = null, bool hidden = false) : base(name, order, displayName, hidden)
+        {
+            int i = 0;
+            _keyValues = keyValues.ToDictionary(x => x, _ => i++);
+        }
+
+        public override void WriteCell(ExcelRange cell, Dictionary<string, PropertyInfo> properties, object? obj)
+        {
+            if (obj == null)
+                return;
+
+            var collection = properties[Name].GetValue(obj)!;
+            if (collection is IDictionary dictionary)
+            {
+                var enumerator = dictionary.GetEnumerator();
+                while(enumerator.MoveNext())
+                {
+                    string key = enumerator.Key.ToString()!;
+                    int offset = _keyValues[key];   //this will throw if key is not in the initialized keyValues. This is intentional
+                    cell.Offset(0, offset).Value = enumerator.Value;
+                }
+            }
+            else if (collection is IEnumerable enumerable)
+            {
+                foreach (object item in enumerable)
+                {
+                    string key = item.ToString()!;
+                    int offset = _keyValues[key];   //this will throw if key is not in the initialized keyValues. This is intentional
+                    cell.Offset(0, offset).Value = item;
+                }
+            } 
+            else
+            {
+                throw new NotSupportedException($"only {nameof(IEnumerable)} or {nameof(IDictionary)} are supported");
+            }
+        }
+    }
+}
